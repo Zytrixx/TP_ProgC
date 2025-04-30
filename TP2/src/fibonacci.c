b062174b3565a3e42428dd67a5dc6675e54d8a5f
Zytrixx <thibault160701@gmail.com>
2025-04-30 16:18:32 +0200

    Update fibonacci.c

---
+++ TP2/src/fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Entrez le nombre de termes de la suite de Fibonacci à afficher : ");
+    scanf("%d", &n);
+
+    int a = 0, b = 1, i;
+
+    for (i = 0; i < n; i++) {
+        if (i == 0)
+            printf("%d", a);
+        else if (i == 1)
+            printf(", %d", b);
+        else {
+            int next = a + b;
+            printf(", %d", next);
+            a = b;
+            b = next;
+        }
+    }
+
+    printf("\n");
+    return 0;
+}
